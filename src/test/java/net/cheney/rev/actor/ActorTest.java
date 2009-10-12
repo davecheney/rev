a7e4e54a637eb391960cbfe5ba33519e18e5d3a8
Dave Cheney <dave@cheney.net>
2009-10-12 21:17:33 +1100

    Actors tests

---
+++ src/test/java/net/cheney/rev/actor/ActorTest.java
@@ -0,0 +1,11 @@
+package net.cheney.rev.actor;
+
+import org.junit.Test;
+
+public class ActorTest {
+
+	@Test
+	public void testActor() {
+		
+	}
+}
---
+++ src/test/java/net/cheney/rev/actor/MessageTest.java
@@ -0,0 +1,11 @@
+package net.cheney.rev.actor;
+
+import org.junit.Test;
+
+public class MessageTest {
+
+	@Test
+	public void messageTest() {
+		
+	}
+}
